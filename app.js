4611f6a58ecb83f7c2d08050a82716a509474465
Triers <luisatriers@gmail.com>
2025-03-17 16:55:03 -0300

    feat: adiciona amigo na listaDeAmigos e funcao limparCampo()

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let listaDeAmigos = [];
+
+// Desenvolva uma função que permita ao usuário inserir um nome no campo de texto e adicioná-lo à lista de amigos criada anteriormente.
+
+function adicionarAmigo(){
+    let amigo = document.getElementById('amigo').value;
+    if (amigo != "") {
+        listaDeAmigos.push(amigo);
+        limparCampo();
+    } else {
+        alert('Por favor, insira um nome.');
+    }
+    console.log(listaDeAmigos);
+}
+
+function limparCampo(){
+    document.querySelector('input').value = '';
+}

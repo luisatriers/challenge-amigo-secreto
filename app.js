a5416fc8b1b1fa505f8b7cae462c47f295d433ba
Triers <luisatriers@gmail.com>
2025-03-17 22:22:08 -0300

    feat: jogo finalizado e código refatorado

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
-
 let amigos = [];
-
-// Desenvolva uma função que permita ao usuário inserir um nome no campo de texto e adicioná-lo à lista de amigos criada anteriormente.
 
 function adicionarAmigo(){
     let amigo = document.getElementById('amigo').value;
@@ -19,11 +15,8 @@ function limparCampo(){
     document.querySelector('input').value = '';
 }
 
-//Crie uma função que percorra o array amigos e adicione cada nome como um elemento <li> dentro de uma lista HTML. Use innerHTML para limpar a lista antes de adicionar novos elementos.
-
-let lista = document.getElementById('listaAmigos');
-
 function mostrarListaNaTela(){
+    let lista = document.getElementById('listaAmigos');
     let itensLista = '';
     for (i = 0 ; i < amigos.length ; i++ ){
         itensLista = itensLista + `<li>${amigos[i]}</li>`;
@@ -31,15 +24,20 @@ function mostrarListaNaTela(){
     lista.innerHTML = itensLista;
 }
 
-// Escreva uma função que selecione aleatoriamente um dos nomes armazenados no array amigos.
+function sortearAmigo(){
+    let quantidadeDeAmigos = amigos.length;
+    // console.log(`A quantidade de amigos na lista é ${quantidadeDeAmigos}`);
 
-// Use Math.random() e Math.floor() para obter um índice aleatório.
-// Validar que há amigos disponíveis: Antes de sortear, verificar se o array amigos não está vazio.
+    let numeroAleatorio = Math.floor(Math.random() * quantidadeDeAmigos);
+    // console.log(`O índice aleatório gerado é ${numeroAleatorio}`);
 
+    let amigoSorteado = amigos[numeroAleatorio];
 
+    if (amigos != '') {
 
-// Gerar um índice aleatório: Usar Math.random() e Math.floor() para selecionar um índice aleatório do array.
-
-// Obter o nome sorteado: Utilizar o índice aleatório para acessar o nome correspondente no array.
-
-// Mostrar o resultado: Atualizar o conteúdo do elemento de resultado usando document.getElementById() e innerHTML para exibir o amigo sorteado.
+        let resultado = document.getElementById('resultado');
+        resultado.innerHTML = `Seu amigo secreto é: ${amigoSorteado}!`;
+    } else {
+        alert('Não foi possível sortear. Por favor, insira alguns nomes antes.');
+    }
+}

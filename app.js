e6ad9d09a34fd21a582140bc2c591eb80b749dd2
Triers <luisatriers@gmail.com>
2025-03-17 21:24:21 -0300

    feat: add funcao mostrar lista de amigos na tela com laco for

--- app.js
+++ app.js
@@ -1,24 +1,18 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 
-let listaDeAmigos = [];
+let amigos = [];
 
 // Desenvolva uma função que permita ao usuário inserir um nome no campo de texto e adicioná-lo à lista de amigos criada anteriormente.
-
 
 function adicionarAmigo(){
     let amigo = document.getElementById('amigo').value;
     if (amigo != '') {
-        listaDeAmigos.push(amigo);
+        amigos.push(amigo);
         limparCampo();
-        contador++;
-
-        mostraLista(contador);
-
     } else {
         alert('Por favor, insira um nome.');
     }
-    // console.log(listaDeAmigos);
-    return contador;
+    mostrarListaNaTela();
 }
 
 function limparCampo(){
@@ -28,16 +22,24 @@ function limparCampo(){
 //Crie uma função que percorra o array amigos e adicione cada nome como um elemento <li> dentro de uma lista HTML. Use innerHTML para limpar a lista antes de adicionar novos elementos.
 
 let lista = document.getElementById('listaAmigos');
-let conteudoLista = '';
-lista.innerHTML = conteudoLista;
-let contador = -1;
 
-function mostraLista(contador){
-    let itemLista = `<li>${listaDeAmigos[contador]}</li>`;
-    conteudoLista = conteudoLista + itemLista;
-    lista.innerHTML = conteudoLista;
+function mostrarListaNaTela(){
+    let itensLista = '';
+    for (i = 0 ; i < amigos.length ; i++ ){
+        itensLista = itensLista + `<li>${amigos[i]}</li>`;
+    }
+    lista.innerHTML = itensLista;
 }
 
+// Escreva uma função que selecione aleatoriamente um dos nomes armazenados no array amigos.
+
+// Use Math.random() e Math.floor() para obter um índice aleatório.
+// Validar que há amigos disponíveis: Antes de sortear, verificar se o array amigos não está vazio.
 
 
 
+// Gerar um índice aleatório: Usar Math.random() e Math.floor() para selecionar um índice aleatório do array.
+
+// Obter o nome sorteado: Utilizar o índice aleatório para acessar o nome correspondente no array.
+
+// Mostrar o resultado: Atualizar o conteúdo do elemento de resultado usando document.getElementById() e innerHTML para exibir o amigo sorteado.
